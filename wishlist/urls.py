fad8f6aadf4a33017ed1c06347a89cd8334afb8a
Rhfialyndra <rahfialyendra5@gmail.com>
2022-09-14 21:30:21 +0700

    init lab 2

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,8 +1,17 @@
-from django.urls import path
-from wishlist.views import show_wishlist
+from django.urls import path;
+from wishlist.views import show_wishlist;
+from wishlist.views import show_wishlist_json;
+from wishlist.views import show_wishlist_xml;
+from wishlist.views import show_wishlist_json_by_id;
+from wishlist.views import show_wishlist_xml_by_id;
+
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('json/', show_wishlist_json, name="show_wishlist_json"),
+    path('xml/', show_wishlist_xml, name="show_wishlist_xml"),
+    path('xml/<int:id>', show_wishlist_xml_by_id, name="show_wishlist_xml_by_id"),
+    path('json/<int:id>', show_wishlist_json_by_id, name="show_wishlist_json_by_id"),
 ]
